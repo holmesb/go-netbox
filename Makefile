2c53fc3439c7b5d9cba881951919c9170e7c95c2
Fabian Breckle <fabian.breckle@breuninger.de>
2020-06-26 23:25:27 +0200

    Add preprocession

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 swagger=docker run --rm -it -e GOPATH=$(HOME)/go:/go -v $(HOME):$(HOME) -w $$(pwd) quay.io/goswagger/swagger:v0.24.0
 generate:
-	$(swagger) generate client --target=./netbox --spec=./swagger.json --copyright-file=./copyright_header.txt
+	$(swagger) generate client --target=./netbox --spec=./swagger.processed.json --copyright-file=./copyright_header.txt
+
+preprocess:
+	bash preprocess.sh swagger.json > swagger.processed.json
 
 clean:
 	rm -rf netbox/client netbox/models
---
+++ preprocess.sh
@@ -0,0 +1,3 @@
+# with_entries(foo) is a shorthand for to_entries | map(foo) | from_entries per https://stedolan.github.io/jq/manual/#to_entries,from_entries,with_entries
+# jq '.definitions.WritableVirtualMachineWithConfigContext.properties = (.definitions.WritableVirtualMachineWithConfigContext.properties | to_entries | map(if .value."x-nullable" then .value."x-omitempty"=false else . end) | from_entries)' swagger.json > bla.json
+jq '.definitions.WritableVirtualMachineWithConfigContext.properties = (.definitions.WritableVirtualMachineWithConfigContext.properties | with_entries(if .value."x-nullable" then .value."x-omitempty"=false else . end))' $1
