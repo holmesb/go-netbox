8560b8d4996348019bfd95ad6755d6e62903ae23
Fabian Breckle <fabian.breckle@breuninger.de>
2020-06-26 17:52:26 +0200

    Test default response

--- netbox/client/virtualization/virtualization_client.go
+++ netbox/client/virtualization/virtualization_client.go
@@ -1150,9 +1150,8 @@ func (a *Client) VirtualizationVirtualMachinesUpdate(params *VirtualizationVirtu
 		return success, nil
 	}
 	// unexpected success response
-	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
-	msg := fmt.Sprintf("unexpected success response for virtualization_virtual-machines_update: API contract not enforced by server. Client expected to get an error, but got: %T", result)
-	panic(msg)
+	unexpectedSuccess := result.(*VirtualizationVirtualMachinesUpdateDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
 // SetTransport changes the transport on the client
--- netbox/client/virtualization/virtualization_virtual_machines_update_responses.go
+++ netbox/client/virtualization/virtualization_virtual_machines_update_responses.go
@@ -21,13 +21,11 @@ package virtualization
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/strfmt"
-	"github.com/go-openapi/swag"
 
 	"github.com/fbreckle/go-netbox/netbox/models"
 )
@@ -46,15 +44,15 @@ func (o *VirtualizationVirtualMachinesUpdateReader) ReadResponse(response runtim
 			return nil, err
 		}
 		return result, nil
-	case 400:
-		result := NewVirtualizationVirtualMachinesUpdateBadRequest()
+	default:
+		result := NewVirtualizationVirtualMachinesUpdateDefault(response.Code())
 		if err := result.readResponse(response, consumer, o.formats); err != nil {
 			return nil, err
 		}
+		if response.Code()/100 == 2 {
+			return result, nil
+		}
 		return nil, result
-
-	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
 	}
 }
 
@@ -91,144 +89,42 @@ func (o *VirtualizationVirtualMachinesUpdateOK) readResponse(response runtime.Cl
 	return nil
 }
 
-// NewVirtualizationVirtualMachinesUpdateBadRequest creates a VirtualizationVirtualMachinesUpdateBadRequest with default headers values
-func NewVirtualizationVirtualMachinesUpdateBadRequest() *VirtualizationVirtualMachinesUpdateBadRequest {
-	return &VirtualizationVirtualMachinesUpdateBadRequest{}
+// NewVirtualizationVirtualMachinesUpdateDefault creates a VirtualizationVirtualMachinesUpdateDefault with default headers values
+func NewVirtualizationVirtualMachinesUpdateDefault(code int) *VirtualizationVirtualMachinesUpdateDefault {
+	return &VirtualizationVirtualMachinesUpdateDefault{
+		_statusCode: code,
+	}
 }
 
-/*VirtualizationVirtualMachinesUpdateBadRequest handles this case with default header values.
+/*VirtualizationVirtualMachinesUpdateDefault handles this case with default header values.
 
-test
+default http response, usually error
 */
-type VirtualizationVirtualMachinesUpdateBadRequest struct {
-	Payload *VirtualizationVirtualMachinesUpdateBadRequestBody
+type VirtualizationVirtualMachinesUpdateDefault struct {
+	_statusCode int
+
+	Payload interface{}
 }
 
-func (o *VirtualizationVirtualMachinesUpdateBadRequest) Error() string {
-	return fmt.Sprintf("[PUT /virtualization/virtual-machines/{id}/][%d] virtualizationVirtualMachinesUpdateBadRequest  %+v", 400, o.Payload)
+// Code gets the status code for the virtualization virtual machines update default response
+func (o *VirtualizationVirtualMachinesUpdateDefault) Code() int {
+	return o._statusCode
 }
 
-func (o *VirtualizationVirtualMachinesUpdateBadRequest) GetPayload() *VirtualizationVirtualMachinesUpdateBadRequestBody {
+func (o *VirtualizationVirtualMachinesUpdateDefault) Error() string {
+	return fmt.Sprintf("[PUT /virtualization/virtual-machines/{id}/][%d] virtualization_virtual-machines_update default  %+v", o._statusCode, o.Payload)
+}
+
+func (o *VirtualizationVirtualMachinesUpdateDefault) GetPayload() interface{} {
 	return o.Payload
 }
 
-func (o *VirtualizationVirtualMachinesUpdateBadRequest) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
-
-	o.Payload = new(VirtualizationVirtualMachinesUpdateBadRequestBody)
+func (o *VirtualizationVirtualMachinesUpdateDefault) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
 	// response payload
-	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
 		return err
 	}
 
-	return nil
-}
-
-/*VirtualizationVirtualMachinesUpdateBadRequestBody virtualization virtual machines update bad request body
-swagger:model VirtualizationVirtualMachinesUpdateBadRequestBody
-*/
-type VirtualizationVirtualMachinesUpdateBadRequestBody struct {
-
-	// error message
-	ErrorMessage string `json:"errorMessage,omitempty"`
-
-	// virtualization virtual machines update bad request body additional properties
-	VirtualizationVirtualMachinesUpdateBadRequestBodyAdditionalProperties map[string]interface{} `json:"-"`
-}
-
-// UnmarshalJSON unmarshals this object with additional properties from JSON
-func (o *VirtualizationVirtualMachinesUpdateBadRequestBody) UnmarshalJSON(data []byte) error {
-	// stage 1, bind the properties
-	var stage1 struct {
-
-		// error message
-		ErrorMessage string `json:"errorMessage,omitempty"`
-	}
-	if err := json.Unmarshal(data, &stage1); err != nil {
-		return err
-	}
-	var rcv VirtualizationVirtualMachinesUpdateBadRequestBody
-
-	rcv.ErrorMessage = stage1.ErrorMessage
-	*o = rcv
-
-	// stage 2, remove properties and add to map
-	stage2 := make(map[string]json.RawMessage)
-	if err := json.Unmarshal(data, &stage2); err != nil {
-		return err
-	}
-
-	delete(stage2, "errorMessage")
-	// stage 3, add additional properties values
-	if len(stage2) > 0 {
-		result := make(map[string]interface{})
-		for k, v := range stage2 {
-			var toadd interface{}
-			if err := json.Unmarshal(v, &toadd); err != nil {
-				return err
-			}
-			result[k] = toadd
-		}
-		o.VirtualizationVirtualMachinesUpdateBadRequestBodyAdditionalProperties = result
-	}
-
-	return nil
-}
-
-// MarshalJSON marshals this object with additional properties into a JSON object
-func (o VirtualizationVirtualMachinesUpdateBadRequestBody) MarshalJSON() ([]byte, error) {
-	var stage1 struct {
-
-		// error message
-		ErrorMessage string `json:"errorMessage,omitempty"`
-	}
-
-	stage1.ErrorMessage = o.ErrorMessage
-
-	// make JSON object for known properties
-	props, err := json.Marshal(stage1)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(o.VirtualizationVirtualMachinesUpdateBadRequestBodyAdditionalProperties) == 0 {
-		return props, nil
-	}
-
-	// make JSON object for the additional properties
-	additional, err := json.Marshal(o.VirtualizationVirtualMachinesUpdateBadRequestBodyAdditionalProperties)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(props) < 3 {
-		return additional, nil
-	}
-
-	// concatenate the 2 objects
-	props[len(props)-1] = ','
-	return append(props, additional[1:]...), nil
-}
-
-// Validate validates this virtualization virtual machines update bad request body
-func (o *VirtualizationVirtualMachinesUpdateBadRequestBody) Validate(formats strfmt.Registry) error {
-	return nil
-}
-
-// MarshalBinary interface implementation
-func (o *VirtualizationVirtualMachinesUpdateBadRequestBody) MarshalBinary() ([]byte, error) {
-	if o == nil {
-		return nil, nil
-	}
-	return swag.WriteJSON(o)
-}
-
-// UnmarshalBinary interface implementation
-func (o *VirtualizationVirtualMachinesUpdateBadRequestBody) UnmarshalBinary(b []byte) error {
-	var res VirtualizationVirtualMachinesUpdateBadRequestBody
-	if err := swag.ReadJSON(b, &res); err != nil {
-		return err
-	}
-	*o = res
 	return nil
 }
