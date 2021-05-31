280dc0ea201f3f6ec564f60629af4c5952f41d7e
Fabian Breckle <fabian.breckle@breuninger.de>
2021-05-31 13:08:49 +0200

    Update README and preprocessor

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,8 @@ logger = logging.getLogger(__name__)
 
 source_file = "swagger.json"
 
-# First, add a default response api call. If these do not exists, we get no error messages from the API.
+# First, add a default response to all api calls. If these do not exists, we get no error messages from the API.
+# This implements https://github.com/netbox-community/go-netbox/commit/674aa060f07457c7dccfc0597e35899448668ef5
 with open(source_file, "r") as readfile:
     data = json.load(readfile)
 
@@ -32,64 +33,66 @@ for path, path_spec in data["paths"].items():
 # Second, fix the config contexts and local context data to our needs
 # This implements https://github.com/fbreckle/go-netbox/commit/987669a91daf2d04a155feaf032a24ed684169b7 and https://github.com/fbreckle/go-netbox/commit/030637bab4bb25cec173035cd2d4a78fb3f47053
 for definition, definition_spec in data["definitions"].items():
-    for property, property_spec in definition_spec["properties"].items():
+    for prop, prop_spec in definition_spec["properties"].items():
         # for config_context, change additionalProperties.type from string to object
-        if property == "config_context":
-            if property_spec["additionalProperties"]["type"] == "string":
+        if prop == "config_context":
+            if prop_spec["additionalProperties"]["type"] == "string":
                 logging.debug(
-                    f"{definition}.{property}.additionalProperties.type == string"
+                    f"{definition}.{prop}.additionalProperties.type == string"
                 )
-                logging.debug("before: " + str(property_spec))
-                property_spec["additionalProperties"] = {"type": "object"}
-                logging.debug("after: " + str(property_spec))
-                logging.info(f"fixed {definition}.{property}")
+                logging.debug("before: " + str(prop_spec))
+                prop_spec["additionalProperties"] = {"type": "object"}
+                logging.debug("after: " + str(prop_spec))
+                logging.info(f"fixed {definition}.{prop}")
 
         # for local_context_data, change type from string to object
         # and add additionalProperties with type object
-        if property == "local_context_data":
-            if property_spec["type"] == "string":
-                logging.debug(f"{definition}.{property}.type == string")
-                logging.debug("before: " + str(property_spec))
-                property_spec["type"] = "object"
-                property_spec["additionalProperties"] = {"type": "object"}
-                logging.debug("after: " + str(property_spec))
-                logging.info(f"fixed {definition}.{property}")
+        if prop == "local_context_data":
+            if prop_spec["type"] == "string":
+                logging.debug(f"{definition}.{prop}.type == string")
+                logging.debug("before: " + str(prop_spec))
+                prop_spec["type"] = "object"
+                prop_spec["additionalProperties"] = {"type": "object"}
+                logging.debug("after: " + str(prop_spec))
+                logging.info(f"fixed {definition}.{prop}")
 
 
 # Third, allow unsetting some optional attributes
 # This implements https://github.com/fbreckle/go-netbox/commit/23f80e06cb3da6f0fd9974a2d986c09826e518b5
-for property, property_spec in data["definitions"]["WritableIPAddress"][
-    "properties"
-].items():
+for prop, prop_spec in data["definitions"]["WritableIPAddress"]["properties"].items():
     if (
-        "x-nullable" in property_spec
-        and property_spec["x-nullable"] == True
-        and property_spec["type"] == "integer"
+        "x-nullable" in prop_spec
+        and prop_spec["x-nullable"] == True
+        and prop_spec["type"] == "integer"
     ):
-        property_spec["x-omitempty"] = False
-        logging.info(f"set x-omitempty = false on WritableIPAddress.{property}")
-    if property == "tags":
-        property_spec["x-omitempty"] = False
-        logging.info(f"set x-omitempty = false on WritableIPAddress.{property}")
+        prop_spec["x-omitempty"] = False
+        logging.info(f"set x-omitempty = false on WritableIPAddress.{prop}")
+    if prop == "tags":
+        prop_spec["x-omitempty"] = False
+        logging.info(f"set x-omitempty = false on WritableIPAddress.{prop}")
 
 # This implements https://github.com/fbreckle/go-netbox/commit/1363e14cfc7bce4bd3d5ee93c09ca70543c51279
-for property, property_spec in data["definitions"][
-    "WritableVirtualMachineWithConfigContext"
-]["properties"].items():
-    if "x-nullable" in property_spec and property_spec["x-nullable"] == True:
-        property_spec["x-omitempty"] = False
+for prop, prop_spec in data["definitions"]["WritableVirtualMachineWithConfigContext"][
+    "properties"
+].items():
+    if "x-nullable" in prop_spec and prop_spec["x-nullable"] == True:
+        prop_spec["x-omitempty"] = False
         logging.info(
-            f"set x-omitempty = false on WritableVirtualMachineWithConfigContext.{property}"
+            f"set x-omitempty = false on WritableVirtualMachineWithConfigContext.{prop}"
         )
-    if property == "tags":
-        property_spec["x-omitempty"] = False
+    if prop == "tags":
+        prop_spec["x-omitempty"] = False
         logging.info(
-            f"set x-omitempty = false on WritableVirtualMachineWithConfigContext.{property}"
+            f"set x-omitempty = false on WritableVirtualMachineWithConfigContext.{prop}"
         )
-
-# todo: check if we actually need https://github.com/fbreckle/go-netbox/commit/7a076f034fc50bb913d5496c193c267e55eb2d2c
-
 
 # Write output file
 with open("swagger.processed.json", "w") as writefile:
     json.dump(data, writefile, indent=2)
+
+
+print("Swaggerfile preprocession complete")
+print("Do not forget to apply the IP Address change at")
+print(
+    "* https://github.com/fbreckle/go-netbox/commit/1363e14cfc7bce4bd3d5ee93c09ca70543c51279#diff-9688c8954edccf890a0b6a8e1bea4d9f81083d0fe1214be0fea2514c0ed3f651 *"
+)
